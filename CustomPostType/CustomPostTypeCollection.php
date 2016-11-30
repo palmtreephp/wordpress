0c731626a93fe87fee0108ebef84b04de04e04b9
andyexeter <palmer.andy@gmail.com>
2016-11-30 10:19:47 +0000

    Use collection key as postType name for custom post type collections

--- CustomPostType/CustomPostTypeCollection.php
+++ CustomPostType/CustomPostTypeCollection.php
@@ -7,4 +7,11 @@ use Palmtree\WordPress\AbstractCollection;
 class CustomPostTypeCollection extends AbstractCollection {
 	protected $itemClass = CustomPostType::class;
 
+	public function addItem( $key, $item ) {
+		if ( is_array( $item ) && ! array_key_exists( 'postType', $item ) ) {
+			$item['postType'] = $key;
+		}
+
+		return parent::addItem( $key, $item );
+	}
 }
